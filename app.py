09f06e0293d6bde89259a1076e67b6ed8da6d209
Francesco Ermini <francescoermini@MacBook-Pro-di-Francesco.local>
2018-09-05 15:55:09 +0200

    Flask web-app first import

---
+++ app.py
@@ -0,0 +1,464 @@
+from flask import Flask, flash, redirect, render_template, request, url_for, jsonify, json
+from models import  db, Curricula, Courses, Groups, Academicyears, GroupsCourses, CurriculaGroups, Students, Studyplans, StudyplansCourses
+from functools import wraps
+from flask_expects_json import expects_json
+from flask_basicauth import BasicAuth
+#from validators import  validateallconstraints, validateconstraints
+#from schemas import allconstraints_schema, curriculum_schema, group_schema, course_schema, courses_schema, constraint_schema, constraints_schema, studyplan_schema
+from flask_cors import CORS, cross_origin
+import csv
+import os
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+POSTGRES = {
+    'user': 'pala',
+    'pw': 'wlavela4ever',
+    'db': 'pianodistudio2',
+    'host': 'localhost',
+    'port': '5432',
+}
+
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+
+app.config['BASIC_AUTH_USERNAME'] = 'admin'
+app.config['BASIC_AUTH_PASSWORD'] = 'admin'
+
+
+
+db.init_app(app)
+db.create_all(app=app)
+
+basic_auth = BasicAuth(app)
+
+@app.route('/')
+def init():
+	return render_template('index.html')
+
+#set manually
+#https://github.com/corydolphin/flask-cors/issues/197
+@app.after_request
+def creds(response):
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+    return response
+
+
+'''		
+method | url                                | input json        | output json       | action
+GET    | /courses/                          |       -           | course_schema     | list all courses
+POST   | /courses/                          | course_schema     | course_schema     | create new courses
+PUT    | 
+DELETE |
+
+GET    | /curriculum/					  	|                   | curricula_schema  | list all curricula
+POST   | /curriculum/					  	| curriculum_schema | curriculum_schema | create a new curriculum
+PUT    | /curriculum/<int:curriculum_id>	| curriculum_schema | curriculum_schema | update a curriculum
+DELETE | /curriculum/<int:curriculum_id>	|					| curriculum_schema | delete a curriculum
+
+GET    | /group/				            |                   | groups_schema     | list all groups
+POST   | /group/			                | groups_schema     | groups_schema     | create a set of groups
+PUT    | /group/<int:group_id>	            | group_schema      | group_schema      | update a group
+DELETE | /group/<int:group_id>           	|				    | group_schema      | delete a group
+
+GET    | /curriculum/<int:curriculum_id>/courses/                                       | 					  	| constraints_schema  |  list the courses constrinats within a curriculum  
+POST   | /curriculum/courses/                                                           | constraints_schema  	| constraints_schema  |  create new courses constraints for a curriculum 
+PUT    | /curriculum/<int:curriculum_id>/course/<string:course_id>/group/<int:group_id>	| constraint_schema  	| constraint_schema   |  update a course constraint 
+DELETE | /curriculum/<int:curriculum_id>/course/<string:course_id>/group/<int:group_id> |					  	| constraint_schema   |  delete a course constraint                              
+
+POST   | /studyplan/						| studyplan_schema |  studyplan_schema | submit the studyplan to validate courses.
+'''
+
+
+@app.route('/academicyear/', methods=['GET'])
+def getAcademicyears():
+    return jsonify({"academicyears":[{ "id": ac.id, "start": ac.start, "end": ac.end } for ac in Academicyears.query.all()]})
+
+@app.route('/academicyear/', methods=['POST'])
+@basic_auth.required
+#@expects_json(curriculum_schema)
+def setAcademicyear():
+	try:
+		data = request.get_json()	
+		academicyear = Academicyears(id=data['id'])
+		academicyear.start = data['start']
+		academicyear.end = data['end']
+		db.session.add(academicyear)
+		db.session.commit()
+	except Exception as e:
+		return jsonify({"error": str(e)}),500
+	return jsonify(data),201
+
+@app.route('/academicyear/<string:academicyear_id>/', methods=['PUT'])
+@basic_auth.required
+#@expects_json(curriculum_schema)
+def updateAcademicyear(academicyear_id):
+	try:
+		data = request.get_json()
+		academicyear = Academicyears.query.get(academicyear_id)
+		academicyear.start = data['start']
+		academicyear.end = data['end']
+		#notice: no check for dupl]icate
+		db.session.commit()
+	except Exception as e:
+		return jsonify({"error": str(e)}),500
+	return jsonify(data),201
+
+@app.route('/academicyear/<string:academicyear_id>/', methods=['DELETE'])
+@basic_auth.required
+def deleteAcademicyear(academicyear_id):	
+	#notice: no check for duplicate
+	try:
+		academicyear = Academicyears.query.get(academicyear_id)
+		db.session.delete(academicyear)
+		db.session.commit()
+	except Exception as e:
+		return jsonify({"errors": academicyear_id, "error": str(e)}),500
+	return jsonify({"id":academicyear_id}),201
+
+
+'''
+
+COURSE
+
+'''
+
+@app.route('/course/', methods=['GET'])
+def getCourses():
+    return jsonify({"courses":[{ "id": c.id, "name":c.name, "cfu":c.cfu, "ac":c.ac, "year":c.year, "semester":c.semester, "ssd":c.ssd, "url":c.url } for c in Courses.query.all()]})
+
+
+@app.route('/course/', methods=['POST'])
+@basic_auth.required
+#@expects_json(course_schema)
+def setCourse():
+	try:
+		d = request.get_json()
+		course = Courses(id=d['id'], name=d['name'], cfu=d['cfu'], ssd=d['ssd'])
+		db.session.add(course)
+		course.ac= d['ac']
+		course.year= d['year']
+		course.semester= d['semester']
+		course.url= d['url']
+		db.session.commit()
+	except Exception as e:
+		return jsonify({"error": str(e)}),500
+	return jsonify(d),201
+
+@app.route('/course/<string:course_id>/', methods=['PUT'])
+@basic_auth.required
+def updateCourse(course_id):
+	try:
+		d = request.get_json()
+		course = Courses.query.get(course_id)
+		course.cfu= d['cfu']
+		course.ssd= d['ssd']
+		course.ac= d['ac']
+		course.year= d['year']
+		course.semester= d['semester']
+		course.url= d['url']
+		db.session.commit()
+	except Exception as e:
+		return jsonify({"error": str(e)}),500
+	return jsonify(d),201
+
+@app.route('/course/<string:course_id>/', methods=['DELETE'])
+#@basic_auth.required
+def deleteCourse(course_id):
+	try:
+		course = Courses.query.get(course_id)
+		db.session.delete(course)
+		db.session.commit()
+	except Exception as e:
+		return jsonify({"error": str(e)}),500
+	return jsonify({"id": str(course_id)}),201
+
+
+'''
+
+CURRICULUM
+
+'''
+
+@app.route('/curriculum/', methods=['GET'])
+def getCurricula():
+    return jsonify({"curricula":[{ "id": c.id, "title":c.title, "desc":c.desc, "ac":c.ac} for c in Curricula.query.order_by(Curricula.id).all()]})
+
+@app.route('/curriculum/', methods=['POST'])
+@basic_auth.required
+#@expects_json(curriculum_schema)
+def setCurriculum():
+	try:
+		data = request.get_json()	
+		curriculum=Curricula(title=data['title'], desc=data['desc'], ac=data['ac'])
+		#for g in Groups.query.filter_by(id=[_id.id for _id in data['groups']])
+		#	curriculum.groups.append(g)
+		db.session.add(curriculum)
+		db.session.commit()
+	except Exception as e:
+		return jsonify({"error": str(e)}),500
+	return jsonify(data),201
+
+@app.route('/curriculum/<int:curriculum_id>/', methods=['PUT'])
+@basic_auth.required
+#@expects_json(curriculum_schema)
+def updateCurriculum(curriculum_id):
+	try:
+		data = request.get_json()
+		curriculum = Curricula.query.get(curriculum_id)
+		curriculum.title = data['title']
+		curriculum.desc = data['desc']
+		curriculum.ac = data['ac']
+		db.session.commit()
+	except Exception as e:
+		return jsonify({"error": str(e)}),500
+	return jsonify(data),201
+
+@app.route('/curriculum/<int:curriculum_id>/', methods=['DELETE'])
+@basic_auth.required
+def deleteCurriculum(curriculum_id):	
+	#notice: no check for duplicate
+	try:
+		curriculum = Curricula.query.get(curriculum_id)
+		db.session.delete(curriculum)
+		db.session.commit()
+	except Exception as e:
+		return jsonify({"error": str(e)}),500
+	return jsonify({"id":curriculum.id, "title":curriculum.title, "desc":curriculum.desc, "ac":curriculum.ac}),201
+
+'''
+
+GROUP
+
+'''
+
+@app.route('/group/', methods=['GET'])
+def getGroups():
+	return jsonify({"groups":[{ "id": g.id,"name":g.name, "cfu":g.cfu, "n":g.n } for g in Groups.query.order_by(Groups.id).all()]})
+
+@app.route('/group/', methods=['POST'])
+@basic_auth.required
+#@expects_json(group_schema)
+def setGroup():
+	try:
+		data = request.get_json()
+		group = Groups(name=data['name'])
+		group.cfu = data['cfu']
+		group.n = data['n']
+		db.session.add(group)
+		db.session.commit()
+	except Exception as e:
+		return jsonify({"error": str(e)}),500
+	return jsonify(data),201
+
+@app.route('/group/<int:group_id>/', methods=['PUT'])
+@basic_auth.required
+#@expects_json(group_schema)
+def updateGroup(group_id):
+	try:
+		data = request.get_json()	
+		group = Groups.query.get(group_id)
+		group.n = data['n']
+		group.cfu = data['cfu']
+		group.name = data['name']
+		db.session.commit()
+	except Exception as e:
+		return jsonify({"error": str(e)}),500
+	return jsonify(data),201
+
+@app.route('/group/<int:group_id>/', methods=['DELETE'])
+@basic_auth.required
+def deleteGroup(group_id):
+	try:
+		group = Groups.query.get(group_id)
+		db.session.delete(group)
+		db.session.commit()
+	except Exception as e:
+		return jsonify({"error": str(e)}),500
+	return jsonify({"id":group.id, "cfu":group.cfu, "n":group.n}),201
+
+
+'''
+
+CONSTRAINTS
+
+
+'''
+@app.route('/group/<int:group_id>/courses/', methods=['GET'])
+def getGroupsCourses(group_id):
+	try:
+		group = Groups.query.get(group_id)
+		
+	except Exception as e:
+		return jsonify({"error": str(e)}),500
+	return jsonify({"id": group.id, "name": group.name, "cfu": group.cfu, "n": group.n, "courses": [{"id": c.id, "name": c.name} for c in group.courses]})
+
+@app.route('/group/<int:group_id>/courses/', methods=['POST'])
+@basic_auth.required
+def setGroupsCourses(group_id):
+	try:
+		data = request.get_json()
+		group = Groups.query.get(group_id)
+		for c in data['courses']:
+
+			course = Courses.query.get(c['id'])
+			group.courses.append(course)
+		db.session.add(group)
+		db.session.commit()
+	except Exception as e:
+		return jsonify({"error": str(e)}),500
+	return jsonify(data),201
+'''
+@app.route('/group/<int:group_id>/course/<string:course_id>/', methods=['PUT'])
+@basic_auth.required
+@expects_json(group_schema)
+def updateGroupsCourses(group_id):
+	try:
+		data = request.get_json()	
+		group = Groups.query.get(group_id)
+
+		group.n = data['n']
+		group.cfu = data['cfu']
+		db.session.commit()
+	except Exception as e:
+		return jsonify({"error": str(e)}),500
+	return jsonify(data),201
+'''
+@app.route('/group/<int:group_id>/courses/<string:course_id>/', methods=['DELETE'])
+@basic_auth.required
+def deleteGroupsCourses(group_id, course_id):
+	try:
+		group = Groups.query.get(group_id)
+		course = Courses.query.get(course_id)
+		group.courses.remove(course);
+		db.session.commit()
+	except Exception as e:
+		return jsonify({"error": str(e)}),500
+	return jsonify({"id": course.id}),201
+
+
+
+
+
+
+@app.route('/curriculum/<int:curriculum_id>/groups/', methods=['GET'])
+def getCurriculumGroups(curriculum_id):
+	try:
+		curriculum = Curricula.query.get(curriculum_id)
+	except Exception as e:
+		return jsonify({"error": str(e)}),500
+	return jsonify({"id": curriculum.id, "title": curriculum.title, "groups": [{"id": g.id, "name": g.name} for g in curriculum.groups]})
+
+@app.route('/curriculum/<int:curriculum_id>/groups/', methods=['POST'])
+@basic_auth.required
+def setCurriculumGroups(curriculum_id):
+	try:
+		data = request.get_json()
+		curriculum = Curricula.query.get(curriculum_id)
+		for g in data['groups']:
+			group = Groups.query.get(g['id'])
+			curriculum.groups.append(group)
+		db.session.add(curriculum)
+		db.session.commit()
+	except Exception as e:
+		return jsonify({"error": str(e)}),500
+	return jsonify(data),201
+
+@app.route('/curriculum/<int:curriculum_id>/groups/<int:group_id>/', methods=['DELETE'])
+@basic_auth.required
+def deleteCurriculumGroups(curriculum_id, group_id):
+	try:
+		curriculum = Curricula.query.get(curriculum_id)
+		group = Groups.query.get(group_id)
+		curriculum.groups.remove(group);
+		db.session.commit()
+	except Exception as e:
+		return jsonify({"error": str(e)}),500
+	return jsonify({"id": group.id}),201
+
+
+
+
+
+
+@app.route('/student/<string:student_id>/', methods=['GET'])
+def getStudent(student_id):
+	try:
+		student = Students.query.get(student_id)
+	except Exception as e:
+		return jsonify({"error": str(e)}),500
+	return jsonify({"id": student.id, "firstname": student.firstname, "lastname": student.lastname}),201
+
+@app.route('/student/', methods=['POST'])
+#@expects_json(group_schema)
+def setStudent():
+	try:
+		data = request.get_json()
+		#if Students.query.get(data['id']) is not None:
+		student = Students(id=data['id'])
+		student.firstname = data['firstname']
+		student.lastname = data['lastname']
+		db.session.add(student)
+		db.session.commit()
+		'''else:
+			student = Students.query.get(data['id'])
+			student.firstname = data['firstname']
+			student.lastname = data['lastname']
+	'''
+	except Exception as e:
+		return jsonify({"error": str(e)}),500
+	return jsonify({"id": student.id, "firstname": student.firstname, "lastname": student.lastname}),201
+'''
+
+@app.route('/student/<string:student_id>/', methods=['DELETE'])
+def deleteStudent(group_id):
+	try:
+		student = Students.query.get(student_id)
+		studyplan = Students.query.get(student_id)
+		db.session.delete(studyplan)
+		db.session.delete(student)
+		db.session.commit()
+	except Exception as e:
+		return jsonify({"error": str(e)}),500
+	return jsonify({"id":group.id, "cfu":group.cfu, "n":group.n}),201
+'''
+
+
+
+
+
+@app.route('/studyplan/', methods=['POST'])
+def setStudyplan():
+	try:
+		data = request.get_json() 
+		student = Students.query.get(data['id']) #note: studyplan id == student id
+		curriculum = Curricula.query.get(data['curriculum'])
+		studyplan = Studyplans(id=student.id, curriculum_id=curriculum.id)
+		for c in data['courses']:
+			course = Courses.query.get(c['id'])
+			studyplan.courses.append(course)
+
+		db.session.add(studyplan)
+		db.session.commit()
+	except Exception as e:
+		return jsonify({"error": str(e)}),500
+	return jsonify(data),201
+
+@app.route('/studyplan/<string:studyplan_id>/', methods=['GET'])
+def getStudyplan(studyplan_id):
+	try: 
+		studyplan = Studyplans.query.get(studyplan_id)
+		student = Students.query.get(studyplan_id)
+		curriculum = Curricula.query.get(studyplan.curriculum_id)
+
+	except Exception as e:
+		return jsonify({"error": str(e)}),500
+	return jsonify({"id": studyplan.id, "student": [{"id": student.id, "firstname": student.firstname, "lastname": student.lastname }], "curriculum": [{"id": curriculum.id, "title": curriculum.title, "ac": curriculum.ac}], "courses": [{"id": c.id, "name": c.name, "ssd": c.ssd, "url": c.url, "cfu": c.cfu, "year": c.year, "semester": c.semester} for c in studyplan.courses ] }),201	
+
+#	return jsonify({"curriculum":curriculum.id, "title":curriculum.title, "groups":[{"id": g.id, "cfu":g.cfu, "n":g.n, "courses": [{"code": k.id, "name": k.name, "ssd": k.ssd, "url":k.url, "cfu": k.credits, "key": k.key} for k in q if k.group_id == g.id]} for g in groups]}),201
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+
+# create a new SQLAlchemy object
+
+db = SQLAlchemy()
+
+# Base model that for other models to inherit from 
+class Base(db.Model):
+	__abstract__ = True
+
+'''
+Groups: Parent
+Course: child
+GroupsCourses: Association
+http://docs.sqlalchemy.org/en/latest/orm/basic_relationships.html#association-object
+
+>>> s = Session()
+>>> group1 = Groups(name='gruppo-inf-inteligent')
+>>> course1 = Courses(id='b000001', name="corso1")
+>>> group1.courses.append(course1)
+>>> s.add(group1)
+>>> s.commit()
+'''
+class Groups(Base):
+    __tablename__ = 'groups'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    n = db.Column(db.Integer, nullable=False)
+    cfu = db.Column(db.Integer)
+
+    courses = db.relationship('Courses', secondary='groups_courses_association')
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return self.name
+
+class Courses(Base):
+    __tablename__ = 'courses'
+    id = db.Column(db.String(7), primary_key=True) # id = B000001
+    name = db.Column(db.String(200), nullable=False)               # name = Analisi Matematica
+    cfu = db.Column(db.Integer, nullable=False)
+    ssd = db.Column(db.String(10), nullable=False)
+    year = db.Column(db.Integer,  nullable=True)
+    semester = db.Column(db.Integer, nullable=True)
+    url = db.Column(db.String(100), nullable=True)
+    ac = db.Column(db.String(9), db.ForeignKey('academicyears.id'))
+    #academicyears = db.relationship('Academicyears',foreign_keys=[ac])
+
+    def __init__(self, id, name, cfu, ssd):
+    	self.id=id
+        self.name = name
+        self.cfu = cfu
+        self.ssd = ssd
+
+class Curricula(Base):
+    __tablename__ = 'curricula'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(500), nullable=False)
+    ac = db.Column(db.String(9), db.ForeignKey('academicyears.id'))
+    desc = db.Column(db.String(2000))
+
+    groups = db.relationship('Groups', secondary='curricula_groups_association')
+
+    def __init__(self, title, ac, desc):
+        self.title = title
+        self.ac = ac
+        self.desc = desc
+
+    def __repr__(self):
+        return self.title
+
+
+class GroupsCourses(Base):
+    __tablename__ = 'groups_courses_association'
+    #id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    group_id = db.Column(db.Integer,  db.ForeignKey('groups.id'),  primary_key=True)
+    course_id = db.Column(db.String(7), db.ForeignKey('courses.id'),  primary_key=True)
+    #group = db.relationship('Groups', foreign_keys=[group_id], backref=db.backref('courses', lazy='dynamic', passive_updates=False))
+    #course = db.relationship('Courses', foreign_keys=[course_id])
+
+    def __init__(self, group_id, course_id):
+        self.course_id = course_id
+        self.group_id = group_id
+
+
+class CurriculaGroups(Base):
+    __tablename__ = 'curricula_groups_association'
+
+    curriculum_id = db.Column(db.Integer, db.ForeignKey('curricula.id'),  primary_key=True)
+    group_id = db.Column(db.Integer,  db.ForeignKey('groups.id'),  primary_key=True)
+
+    def __init__(self, curriculum_id, group_id):
+        self.curriculum_id = curriculum_id
+        self.group_id = group_id
+
+'''
+
+class Constraints(Base):
+    __tablename__ = 'constraints'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    curriculum_id = db.Column(db.Integer, db.ForeignKey('curricula.id'))
+    course_id = db.Column(db.String(7))
+    group_id = db.Column(db.Integer)
+    status = db.Column(db.Boolean) # to mark poll as open or closed
+
+	# Relationship declaration (makes it easier for us to access the polls model
+	# from the other models it's related to)
+	#curriculum = db.relationship('Curricula', foreign_keys=[curriculum_id],
+	#	backref=db.backref('courses', lazy='dynamic',passive_updates=False))
+
+	#course = db.relationship('Courses',foreign_keys=[course_id],
+	#	backref=db.backref('constraints', lazy='dynamic', passive_updates=False))
+
+    def __init__(self, curriculum_id, group_id, course_id):
+    	self.curriculum_id=curriculum_id
+    	self.group_id=group_id
+    	self.course_id=course_id
+
+    def __repr__(self):
+    	return self.course.name
+
+'''
+class Students(Base):
+    __tablename__ = 'students'
+    id = db.Column(db.String(7), primary_key=True)
+    firstname = db.Column(db.String(20))
+    lastname = db.Column(db.String(20))
+
+    def __init__(self, id):
+        self.id = id
+    def __repr__(self):
+        return self.id
+
+
+class Studyplans(Base):
+    __tablename__ = 'studyplans'
+    id = db.Column(db.String(7),  db.ForeignKey('students.id'),  primary_key=True)
+    curriculum_id = db.Column(db.Integer, db.ForeignKey('curricula.id'))
+
+    courses = db.relationship('Courses', secondary='studyplan_courses_assosiation')
+
+    def __init__(self, id, curriculum_id):
+        self.id = id
+        self.curriculum_id = curriculum_id
+
+    def __repr__(self):
+        return self.id
+
+class StudyplansCourses(Base):
+    __tablename__ = 'studyplan_courses_assosiation'
+    studyplan_id = db.Column(db.String(7), db.ForeignKey('studyplans.id'), primary_key=True)
+    course_id = db.Column(db.String(7), db.ForeignKey('courses.id'),  primary_key=True)
+
+    def __init__(self, studyplan_id, course_id):
+        self.course_id=course_id
+        self.studyplan_id=studyplan_id
+
+
+class Academicyears(Base):
+    __tablename__ = 'academicyears'
+    id = db.Column(db.String(9), primary_key=True )
+    start = db.Column(db.DateTime)
+    end = db.Column(db.DateTime)
+
+    def __init__(self, id):
+    	self.id=id
+
+    def __repr__(self):
+    	return self.id
